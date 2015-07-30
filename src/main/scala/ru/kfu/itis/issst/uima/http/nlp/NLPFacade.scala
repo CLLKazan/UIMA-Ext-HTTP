307dc0786ccff39fb314835d24b386dbe8530e30
Rinat Gareev <gareev-rm@yandex.ru>
2015-07-30 17:17:15 +0300

    Added lemmatizer into the pipeline. Response also includes an additional column with a word's lemma

--- src/main/scala/ru/kfu/itis/issst/uima/http/nlp/NLPFacade.scala
+++ src/main/scala/ru/kfu/itis/issst/uima/http/nlp/NLPFacade.scala
@@ -3,39 +3,33 @@
  */
 package ru.kfu.itis.issst.uima.http.nlp
 
-import akka.actor.Actor
-import akka.actor.ActorLogging
-import org.apache.uima.fit.factory.ExternalResourceFactory
-import org.apache.uima.fit.util.JCasUtil
-import ru.kfu.itis.issst.uima.morph.commons.{GramModelBasedTagMapper, TagAssembler}
-import ru.kfu.itis.issst.uima.morph.dictionary.MorphDictionaryAPIFactory.getMorphDictionaryAPI
-import ru.kfu.itis.issst.uima.morph.dictionary.{MorphDictionaryAPIFactory, MorphDictionaryAPI}
-import ru.kfu.itis.issst.uima.postagger.{PosTaggerAPI, MorphCasUtils}
-import ru.kfu.itis.issst.uima.tokenizer.TokenizerAPI
-import ru.kfu.itis.issst.uima.segmentation.SentenceSplitterAPI
-import org.apache.uima.resource.metadata.impl.Import_impl
-import ru.kfu.itis.cll.uima.util.PipelineDescriptorUtils
-import scala.collection.JavaConversions._
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Status, actorRef2Scala}
 import org.apache.uima.UIMAFramework
 import org.apache.uima.analysis_engine.AnalysisEngine
-import akka.actor.Status
+import org.apache.uima.fit.factory.ExternalResourceFactory
+import org.apache.uima.fit.util.JCasUtil
 import org.apache.uima.jcas.JCas
-import ru.kfu.cll.uima.segmentation.fstype.Sentence
-import org.opencorpora.cas.Word
-import ru.kfu.cll.uima.tokenizer.fstype.Token
-import akka.actor.ActorRef
-import akka.actor.Props
-import akka.actor.actorRef2Scala
 import org.opencorpora.cas.Word
 import ru.kfu.cll.uima.segmentation.fstype.Sentence
 import ru.kfu.cll.uima.tokenizer.fstype.Token
+import ru.kfu.itis.cll.uima.util.PipelineDescriptorUtils
+import ru.kfu.itis.issst.uima.morph.commons.{GramModelBasedTagMapper, TagAssembler}
+import ru.kfu.itis.issst.uima.morph.dictionary.MorphDictionaryAPIFactory.getMorphDictionaryAPI
+import ru.kfu.itis.issst.uima.morph.lemmatizer.LemmatizerAPI
+import ru.kfu.itis.issst.uima.postagger.{MorphCasUtils, PosTaggerAPI}
+import ru.kfu.itis.issst.uima.segmentation.SentenceSplitterAPI
+import ru.kfu.itis.issst.uima.tokenizer.TokenizerAPI
+
+import scala.collection.JavaConversions._
 
 /**
  * @author Rinat Gareev (Kazan Federal University)
  *
  */
 class NLPFacade extends Actor with ActorLogging {
+
   import NLPFacade._
+
   val nlpWorker: ActorRef = context.actorOf(Props[NLPWorker])
   val maxQueueSize = 10
   val taskQueue = scala.collection.mutable.Queue.empty[(ActorRef, NLPTask)]
@@ -63,16 +57,16 @@ class NLPWorker extends Actor with ActorLogging {
   private var cas: JCas = null
 
   override def preStart() {
-    val gramModelDesc = getMorphDictionaryAPI.getGramModelDescription()
-    val tagAssemblerDesc = TagAssembler.createDescription()
-    ExternalResourceFactory.bindExternalResource(tagAssemblerDesc,
-      GramModelBasedTagMapper.RESOURCE_GRAM_MODEL, gramModelDesc)
     val analyzers = Map(
       "tokenizer" -> TokenizerAPI.getAEImport,
       "sentenceSplitter" -> SentenceSplitterAPI.getAEImport,
       "pos-tagger" -> PosTaggerAPI.getAEImport,
-      "pos-tag-assembler" -> tagAssemblerDesc)
+      "lemmatizer" -> LemmatizerAPI.getAEImport)
     val aggregateDesc = PipelineDescriptorUtils.createAggregateDescription(analyzers)
+    val morphDictDesc = getMorphDictionaryAPI.getResourceDescriptionForCachedInstance
+    morphDictDesc.setName(PosTaggerAPI.MORPH_DICTIONARY_RESOURCE_NAME)
+    PipelineDescriptorUtils.getResourceManagerConfiguration(aggregateDesc)
+      .addExternalResource(morphDictDesc)
     engine = UIMAFramework.produceAnalysisEngine(aggregateDesc)
     cas = engine.newJCas()
   }
@@ -102,10 +96,15 @@ class NLPWorker extends Actor with ActorLogging {
 }
 
 object NLPFacade {
+
   trait NLPTask
+
   case class PerformPosTagging(text: String) extends NLPTask
+
   private[nlp] case object WorkAvailable
+
   private[nlp] case object GimmeWork
+
   private[nlp] case class DelegatedNLPTask(client: ActorRef, task: NLPTask)
 
   private[nlp] def toPlainText(jCas: JCas): String = {
@@ -114,13 +113,16 @@ object NLPFacade {
       yield word.getToken -> word).toMap
     for (sentence <- JCasUtil.select(jCas, classOf[Sentence])) {
       for (token <- JCasUtil.selectCovered(jCas, classOf[Token], sentence)) {
-        result.append(escapeToken(token.getCoveredText()))
+        result.append(escapeToken(token.getCoveredText))
         result.append('\t')
-        result.append(token.getType().getShortName())
+        result.append(token.getType.getShortName)
         token2WordIdx.get(token) match {
           case Some(word) =>
+            val wf = MorphCasUtils.requireOnlyWordform(word)
             result.append('\t')
-            result.append(MorphCasUtils.requireOnlyWordform(word).getPos)
+            result.append(wf.getLemma)
+            result.append('\t')
+            result.append(wf.getPos)
           case None =>
         }
         // token delimiter
@@ -129,7 +131,7 @@ object NLPFacade {
       // sentence delimiter
       result.append('\n')
     }
-    result.toString
+    result.toString()
   }
 
   private def escapeToken(txt: String): String = txt.replaceAll("[\t\n\r]+", " ")
